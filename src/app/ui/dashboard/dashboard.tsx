f858727ba1ac7f60d161b6fcb871c8c388db3834
phtevesn <steveninouye@csus.edu>
2025-10-22 22:07:29 -0700

    added: test components for inventory tabs

--- src/app/ui/dashboard/dashboard.tsx
+++ src/app/ui/dashboard/dashboard.tsx
@@ -6,6 +6,7 @@ import {useRouter} from 'next/navigation';
 import {getUserStuff} from "@/app/routing/dashboard/dashboard";
 import {logoutUser} from "@/app/utils/users/logout"
 import CreateInventory from "@/app/ui/inventory/create"
+import InvTab from '@/app/ui/inventory/invtab';
 
 export default function Dashboard(){
   const router = useRouter();
@@ -31,6 +32,16 @@ export default function Dashboard(){
     return <div>Loading ma boi gimme a seccy</div>;
   }
   console.log(user.id)
+  
+
+  const sampo_data = [  //temp data
+    { invID: 1, invName: "Groceries" },
+    { invID: 2, invName: "Electronics" },
+    { invID: 3, invName: "Clothes" },
+    { invID: 4, invName: "Books" },
+    { invID: 5, invName: "Furniture" },
+  ];
+
 
   return (
     <div>
@@ -44,6 +55,14 @@ export default function Dashboard(){
         {!showForm && <button onClick={createInv} className="auth-button rounded-full mt-4"> create inventory </button>}
         {showForm && <CreateInventory />}
       </div>
+            <div className="flex flex-col justify-center items-center mt-10 space-y-4">
+        {sampo_data.map((inv) => (
+          <div key={inv.invID}>
+            <InvTab invID={inv.invID} invName={inv.invName} />
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 }
