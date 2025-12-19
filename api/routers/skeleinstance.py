5a54ec0168e2a51b29c17d34ac0c86c27c7b7b7d
phtevesn <steveninouye@csus.edu>
2025-12-18 15:36:32 -1000

    Added: finished skeleinstance functionality

--- api/routers/skeleinstance.py
+++ api/routers/skeleinstance.py
@@ -25,7 +25,30 @@ def createSkeleInstance(instance_info: SkeleInstanceInfo, cur_user: Users = Depe
   return skele_instance
 
 @router.put("/instance/edit")
-
+def editSkeleInstance(instance_id: int, folder_id: int, count: int, db: Session = Depends(get_db)):
+  skele_instance = edit_skele_instance(instance_id, folder_id, count, db)
+  if not skele_instance:
+    raise HTTPException(
+      status_code=status.HTTP_400_BAD_REQUEST,
+      detail=f"Failed to edit skele instance with id: {instance_id}"
+    )
+  return skele_instance
+                                       
 @router.delete("/instance/delete")
+def deleteSkeleInstance(instance_id: int, db: Session = Depends(get_db)):
+  if not deleteSkeleInstance(instance_id, db):
+    raise HTTPException(
+      status_code = status.HTTP_400_BAD_REQUEST,
+      detail=f"Failed to delete skele instance with id: {instance_id}"
+    )
+  return True
 
 @router.get("/instances/get")
+def getSkeleInstances(inv_id: int, db:Session = Depends(get_db)):
+  instances = get_skele_instances(inv_id, db)
+  if not instances:
+    raise HTTPException(
+      status_code = status.HTTP_404_NOT_FOUND,
+      detail = f"No instances for inventory with id: {inv_id}"
+    )
+  return instances
